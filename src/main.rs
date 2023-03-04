45ac9e7b320de580222721b2f5ec8346a18e994d
-Lunat1c- <arturgrabowski100@onet.pl>
2023-03-04 12:54:12 +0100

    Input, debugging, splitted project into plugins

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+#![allow(clippy::redundant_field_names)]
 use bevy::{prelude::*, render::camera::ScalingMode};
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
+
+mod player;
+mod debug;
+mod ascii;
+
+use player::PlayerPlugin;
+use debug::DebugPlugin;
+use ascii::AsciiPlugin;
 
 fn main() {
     App::new()
@@ -14,47 +24,12 @@ fn main() {
         resizable: false,
         ..Default::default()
     })
-    .add_startup_system(spawn_camera)
-    .add_startup_system(spawn_player)
-    .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
+    .add_startup_system(spawn_camera)    
     .add_plugins(DefaultPlugins)
+    .add_plugin(PlayerPlugin)
+    .add_plugin(AsciiPlugin)
+    .add_plugin(DebugPlugin)
     .run();
-}
-
-fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>){
-    let mut sprite = TextureAtlasSprite::new(1);
-    sprite.color = Color::rgb(0.3, 0.3, 0.3);
-    sprite.custom_size = Some(Vec2::splat(1.0));
-
-    let player = commands.spawn_bundle(SpriteSheetBundle {
-        sprite: sprite,
-        texture_atlas: ascii.0.clone(),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 900.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(Name::new("Player"))
-    .id();
-
-    let mut background_sprite = TextureAtlasSprite::new(1);
-    background_sprite.color = Color::rgb(1.0, 1.0, 1.0);
-    background_sprite.custom_size = Some(Vec2::splat(1.0));
-
-    let background = commands.spawn_bundle(SpriteSheetBundle {
-        sprite: background_sprite,
-        texture_atlas: ascii.0.clone(),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, -1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(Name::new("Background"))
-    .id(); //id() gives back the entity after creation
-
-    commands.entity(player).push_children(&[background]);
 }
 
 fn spawn_camera(mut commands: Commands) {
@@ -69,21 +44,4 @@ fn spawn_camera(mut commands: Commands) {
     camera.orthographic_projection.scaling_mode = ScalingMode::None;
 
     commands.spawn_bundle(camera);
-}
-
-struct AsciiSheet(Handle<TextureAtlas>);
-
-fn load_ascii(mut commands: Commands, assets: Res<AssetServer>, mut texture_atlasses: ResMut<Assets<TextureAtlas>>) {
-    let image = assets.load("Ascii.png");
-    let atlas = TextureAtlas::from_grid_with_padding(
-        image,
-        Vec2::splat(9.0),
-        16,
-        16,
-        Vec2::splat(2.0)
-    );
-
-    let atlas_handle = texture_atlasses.add(atlas);
-
-    commands.insert_resource(AsciiSheet(atlas_handle));
 }
---
+++ src/player.rs
@@ -0,0 +1,73 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::Inspectable;
+
+use crate::{ascii::AsciiSheet, TILE_SIZE, ascii::spawn_ascii_sprite};
+
+pub struct PlayerPlugin;
+
+#[derive(Component, Inspectable)]
+pub struct Player{
+    speed: f32
+}
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_player)
+            .add_system(player_movement);
+    }
+}
+
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>
+) {
+    // TODO change single_mut() to get_single_mut() for error handling
+    let (player, mut transform) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::W){
+        transform.translation.y += player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::S){
+        transform.translation.y -= player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::A){
+        transform.translation.x -= player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::D){
+        transform.translation.x += player.speed * TILE_SIZE * time.delta_seconds();
+    }
+}
+
+fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>){
+
+    let player = spawn_ascii_sprite(
+        &mut commands, 
+        &ascii, 
+        1,
+        Color::rgb(0.3, 0.3, 0.3),
+        Vec3::new(0.0, 0.0, 900.0)
+    );
+
+    commands
+        .entity(player)
+        .insert(Name::new("Player"))
+        .insert(Player {
+            speed: 3.0
+        })
+        .id();
+
+    let background = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        0, 
+        Color::rgb(0.5, 0.5, 0.5), 
+        Vec3::new(0.0, 0.0, -1.0)
+    );
+
+    commands.entity(background)
+        .insert(Name::new("Background"))
+        .id(); //id() gives back the entity after creation
+
+    commands.entity(player).push_children(&[background]);
+}
